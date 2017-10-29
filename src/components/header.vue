9b72e2dcacdbcdae6e7a82f8176e6c922e9d066f
Sergey <sunliveua@gmail.com>
2017-10-29 14:01:40 +0200

    0.1

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,18 +1,29 @@
 <template>
   <header>
     <v-container fluid class="header">
-      <v-layout justify-center>
+      <v-layout class="pt-4" justify-center align-start>
         <img src="/public/img/logo.png" alt="logo">
+      </v-layout>
+      <v-layout justify-center class="my-3">
+        <div class="border"></div>
+      </v-layout>
+      <v-layout justify-center>
+        <ul class="menu">
+          <li>Главная</li>
+          <li>Особенности</li>
+          <li>Состав</li>
+          <li>Купить</li>
+        </ul>
+      </v-layout>
+      <v-layout justify-center align-center class="something">
+        <v-flex xs12 sm10 md8 lg6 text-xs-center>
+          <h2 class="head">Идеальное решение на осень.</h2>
+          <p class="mb-0">В этом году холода наступили так быстро, что многие неуспели к ним хорошенько подготовиться. А теперь уже вроде бы поздно. Все хорошие вещи скупили, выходить из дома не хочется, а мороз все ближе. Но есть одно любопытное решение. Какое?</p>
+        </v-flex>
       </v-layout>
     </v-container>
   </header>
 </template>
-
-<script>
-  export default {
-    
-  }
-</script>
 
 <style scoped lang="stylus">
 .header
@@ -20,4 +31,25 @@
   background-size cover
   background-repeat no-repeat
   height 735px
+  .menu
+    display flex
+    li
+      text-transform uppercase
+      margin 0 1.5rem
+      list-style none
+      color #fff
+      font-family Arial, Helvetica, sans-serif
+  .something
+    height 80%
+    p
+      color #fff
+      font-size 1rem
+      width 80%
+      margin 0 auto
+  
+  .border
+    width 25%
+    height 1px
+    background-color #fff
+    opacity .4
 </style>
